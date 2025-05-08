0f7e4d81e258b583d941c6a5b80353d39f3648d6
Matthias Hochsteger <mhochsteger@cerbsim.com>
2025-05-08 20:28:50 +0200

    page

---
+++ assets/pinia-ChsewFY9.js
@@ -0,0 +1,1 @@
+import{b as i}from"./index-3fevySbJ.js";import{c as t}from"./pinia-Cz-fGEgD.js";import"./index-biZAYMy_.js";const a=t(),e=i(({app:o})=>{o.use(a)});export{e as default,a as pinia};
---
+++ assets/use-quasar-ThZkhwN7.js
@@ -0,0 +1,1 @@
+import{i as a,bE as r}from"./index-biZAYMy_.js";function u(){return a(r)}export{u};
